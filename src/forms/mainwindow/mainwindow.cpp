4b1f2772ff0701105d8106cc12b35d0c3f64ab77
songd <izwb003@gmail.com>
2024-02-21 19:36:00 +0800

    Linked ffmpeg.

--- src/forms/mainwindow/mainwindow.cpp
+++ src/forms/mainwindow/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QDebug>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    auto avversion = av_version_info();
+    qDebug()<<avversion;
 }
 
 MainWindow::~MainWindow()
